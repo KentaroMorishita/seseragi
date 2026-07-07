140faa33d6d8483c2c3ff0f2c90db77d1ac5a9f7
morishita <qqfhy591@gmail.com>
2026-07-07 19:44:23 +0900

    syntax: reuse source span model

--- crates/seseragi-syntax/src/lib.rs
+++ crates/seseragi-syntax/src/lib.rs
@@ -6,6 +6,6 @@ mod token;
 
 pub use cst::{parse_cst, CstArtifact, CstError, CstMissing, CstNode};
 pub use lexer::lex;
-pub use source::SourceSnapshot;
+pub use source::{LineColumn, LineIndex, SourceSnapshot, Span};
 pub use surface::{parse_surface_ast, ByteSpan, SurfaceDecl, SurfaceModule, TypeRef, Visibility};
 pub use token::{Token, TokenKind, TokenStream};
