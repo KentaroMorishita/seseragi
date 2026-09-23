03f410a38b3b4e63b9fc4e9e78d083c3fb3dc585
morishita <qqfhy591@gmail.com>
2026-09-23 15:55:20 +0900

    fixtures: sync v0.61.12 release contracts

--- examples/spec/artifacts/project-schema-1/transitive-effect-failure/artifacts/main/main.ts
+++ examples/spec/artifacts/project-schema-1/transitive-effect-failure/artifacts/main/main.ts
@@ -1,5 +1,6 @@
 import { rejectViaFacade } from "./facade.js"
 import { type InputError } from "./provider.js"
+import "./provider.js"
 import { assertUnicodeVersion as $ssrg$assertUnicodeVersion } from "@seseragi/runtime/unicode-version"
 import { flatMap as _ssrg_effect_flatMap, succeed as _ssrg_effect_succeed } from "@seseragi/runtime/effect"
 $ssrg$assertUnicodeVersion("17.0.0")
